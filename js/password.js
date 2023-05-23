ddfdcf999df8714da3d09b786e91365e19444820
liqiangnie <51591876+liqiangnie@users.noreply.github.com>
2023-05-23 16:26:39 +0800

    Add files via upload

---
+++ js/password.js
@@ -0,0 +1,19 @@
+function checkPassword() {
+  var passwordInput = document.getElementById("password");
+  var password = passwordInput.value;
+  
+  // Replace "password123" with your desired password
+  if (password === "fulldata123") {
+    var hiddenRows = document.querySelectorAll(".hidden_row");
+    hiddenRows.forEach(function(row) {
+      row.classList.remove("hidden_row");
+    });
+    
+    document.getElementById("passwordInput").style.display = "none";
+  } else {
+    alert("Incorrect password. Please try again.");
+  }
+  
+  // Clear the password input field
+  passwordInput.value = "";
+}
